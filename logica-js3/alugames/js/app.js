72ce16428c14094f10e4c17426be717948d8c951
Pedro <p3dr0sk0sk7160@gmail.com>
2025-10-09 17:32:11 -0300

    adicionar confirmador de devolução

--- logica-js3/alugames/js/app.js
+++ logica-js3/alugames/js/app.js
@@ -3,15 +3,23 @@ function alterarStatus (id) {
    let statusAlugado = document.getElementById (`game-${id}`);
    let statusImagem = statusAlugado.querySelector (".dashboard__item__img");
    let statusBotao = statusAlugado.querySelector (".dashboard__item__button");
+   let confirmarDevolucao;
 
    if (statusImagem.classList.contains ("dashboard__item__img--rented")) {
+    
+      confirmarDevolucao = prompt ('Deseja realmente devolver o jogo? Digite "Sim".');
+    
+    if (confirmarDevolucao !== "Sim") {
+      return;
+    }
+    
     statusImagem.classList.remove ("dashboard__item__img--rented");
     statusBotao.classList.remove ("dashboard__item__button--return");
     statusBotao.textContent = "Alugar";  
    } else {
     statusImagem.classList.add ("dashboard__item__img--rented");
     statusBotao.classList.add ("dashboard__item__button--return");
-    statusBotao.textContent = "Devolver"; 
+    statusBotao.textContent = "Devolver";
    }
 
 } 
