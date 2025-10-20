a11c9a87eda3697fae9a9d67135c569adddc6a23
Pedro <p3dr0sk0sk7160@gmail.com>
2025-10-19 21:55:50 -0300

    recuperar valores carrinho de compras

---
+++ logica-js3/carrinho-compras/js/app.js
@@ -0,0 +1,13 @@
+function adicionar () {
+ 
+ let nomeProduto = document.getElementById ('produto');
+ let valorProduto = document.getElementById ('produto');
+ let quantidadeProdutos = document.getElementById ('quantidade').value;
+ let carrinhoProdutos = document.getElementById ('lista-produtos');
+ let carrinhoTotal = document.getElementById ('valor-total');
+  
+}
+
+function limpar () {
+
+}
