81d94febca876b6e5dbf8ed568ecf7457006eca2
Pedro <p3dr0sk0sk7160@gmail.com>
2025-10-21 14:49:39 -0300

    resolvendo a função adicionar (não terminado)

--- logica-js3/carrinho-compras/js/app.js
+++ logica-js3/carrinho-compras/js/app.js
@@ -1,12 +1,25 @@
+let listaPrecos = [];
+
 function adicionar () {
  
- let nomeProduto = document.getElementById ('produto');
- let valorProduto = document.getElementById ('produto');
+ let produto = document.getElementById ('produto').value;
+ let nomeProduto = produto.split ('-') [0];
+ let valorProduto = produto.split ('R$') [1];
  let quantidadeProdutos = document.getElementById ('quantidade').value;
+ let preco = quantidadeProdutos * valorProduto;
+ 
  let carrinhoProdutos = document.getElementById ('lista-produtos');
+ carrinhoProdutos.innerHTML += (`<span class="texto-azul">${quantidadeProdutos}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>`);
+ listaPrecos.push (preco);
+ 
+ let somaPrecos = listaPrecos.reduce ((a, b) => a + b, 0);
+
  let carrinhoTotal = document.getElementById ('valor-total');
-  
+ carrinhoTotal.textContent = (`R$${somaPrecos}`);
+
 }
+
+carrinhoTotal.textContent = (`R$${preco}`);
 
 function limpar () {
 
